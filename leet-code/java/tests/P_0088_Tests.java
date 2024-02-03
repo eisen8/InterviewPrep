0052b50ec267a829919d61ee2216500500262a36
Eisen <eisene@protonmail.com>
2024-02-02 20:23:17 -0800

    Adding LeetCode problem 88 Merge Sort Array

---
+++ leet-code/java/tests/P_0088_Tests.java
@@ -0,0 +1,62 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class P_0088_Tests {
+    @Test
+    public void merge_e1() {
+        P_0088 CUT = new P_0088();
+        int[] n1 = {1, 2, 3, 0, 0, 0};
+        int[] n2 = {2, 5, 6};
+        int[] expected = {1, 2, 2, 3, 5, 6};
+
+        CUT.merge(n1, 3, n2, 3);
+
+        Assert.assertArrayEquals(expected, n1);
+    }
+
+    @Test
+    public void merge_e2() {
+        P_0088 CUT = new P_0088();
+        int[] n1 = {1};
+        int[] n2 = {};
+        int[] expected = {1};
+
+        CUT.merge(n1, 1, n2, 0);
+
+        Assert.assertArrayEquals(expected, n1);
+    }
+
+    @Test
+    public void merge_e3() {
+        P_0088 CUT = new P_0088();
+        int[] n1 = {0};
+        int[] n2 = {1};
+        int[] expected = {1};
+
+        CUT.merge(n1, 0, n2, 1);
+
+        Assert.assertArrayEquals(expected, n1);
+    }
+
+/*  Example 1:
+
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Output: [1,2,2,3,5,6]
+    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+    Example 2:
+
+    Input: nums1 = [1], m = 1, nums2 = [], n = 0
+    Output: [1]
+    Explanation: The arrays we are merging are [1] and [].
+    The result of the merge is [1].
+
+    Example 3:
+
+    Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+    Output: [1]
+    Explanation: The arrays we are merging are [] and [1].
+    The result of the merge is [1].
+    Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.*/
+}

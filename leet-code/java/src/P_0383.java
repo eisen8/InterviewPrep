88fee62c5adeaf861730bd9c97302c7ac24369d7
Eisen <eisene@protonmail.com>
2024-02-14 07:39:48 -0800

    Adding second solution to Problem 383

--- leet-code/java/src/P_0383.java
+++ leet-code/java/src/P_0383.java
@@ -9,10 +9,10 @@ public class P_0383 {
      * This solution stores the count of available characters (from magazine) into a map. Then it goes through the ransomNote
      * decrementing the available count of each character.
      * Time complexity: O(n + m)
-     * Space complexity: O(n + m)
+     * Space complexity: O(1)
      *
      * Notes: Hashing can be slow. Since only english lowercase letters are allowed, it would be faster to use a 26 item array
-     * for the counts and use the char to index into the array (i.e. 'a' = 0, 'z' = 25).
+     * for the counts and use the char to index into the array (i.e. 'a' = 0, 'z' = 25). See below canConstruct2 method.
      */
     public boolean canConstruct(String ransomNote, String magazine) {
         if (ransomNote == null || magazine == null || ransomNote.length() > magazine.length()) {
@@ -28,11 +28,44 @@ public class P_0383 {
 
         // Verify enough counts for ransom Note
         for (char c : ransomNote.toCharArray()) {
-            int count = charCounts.getOrDefault(c, 0) - 1;
-            if (count < 0) {
+            int newCount = charCounts.getOrDefault(c, 0) - 1;
+            if (newCount < 0) {
                 return false;
             } else {
-                charCounts.put(c, count);
+                charCounts.put(c, newCount);
+            }
+        }
+
+
+        return true;
+    }
+
+    /**
+     * Same as above but uses an array as a "hash map"; the index corresponds to the character (0-25) and the value the count.
+     * Time complexity: O(n + m)
+     * Space complexity: O(1)
+     */
+    public boolean canConstruct2(String ransomNote, String magazine) {
+        if (ransomNote == null || magazine == null || ransomNote.length() > magazine.length()) {
+            return false; // fast fail
+        }
+
+        int[] charCounts = new int[26]; // simple/fast 'hash map'. Index corresponds to character; value to count
+
+        // Add mag chars to map
+        for (char c : magazine.toCharArray()) {
+            int index = c - 'a';
+            charCounts[index] += 1;
+        }
+
+        // Verify enough counts for ransom Note
+        for (char c : ransomNote.toCharArray()) {
+            int index = c - 'a';
+            int newCount = charCounts[index]-1;
+            if (newCount < 0) {
+                return false;
+            } else {
+                charCounts[index] = newCount;
             }
         }
 
--- leet-code/java/tests/P_0383_Tests.java
+++ leet-code/java/tests/P_0383_Tests.java
@@ -37,4 +37,37 @@ public class P_0383_Tests {
         Assert.assertTrue(result);
     }
 
+    @Test
+    public void canConstruct2_e1() {
+        String ran = "a";
+        String mag = "b";
+        P_0383 CUT = new P_0383();
+
+        boolean result  = CUT.canConstruct2(ran, mag);
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void canConstruct2_e2() {
+        String ran = "aa";
+        String mag = "ab";
+        P_0383 CUT = new P_0383();
+
+        boolean result  = CUT.canConstruct2(ran, mag);
+
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void canConstruct2_e3() {
+        String ran = "aa";
+        String mag = "aab";
+        P_0383 CUT = new P_0383();
+
+        boolean result  = CUT.canConstruct2(ran, mag);
+
+        Assert.assertTrue(result);
+    }
+
 }

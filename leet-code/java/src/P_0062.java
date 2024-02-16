91b511492f674112bd66bf3c428d160e0ee3d1b5
Eisen <eisene@protonmail.com>
2024-02-16 04:43:45 -0800

    Adding LeetCode problem 62: unique paths

---
+++ leet-code/java/src/P_0062.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+// 62. Unique Paths --- https://leetcode.com/problems/unique-paths/description/
+// See bottom for problem statement
+public class P_0062 {
+
+    /**
+     * Recursively go through each path
+     */
+    public int uniquePaths(int m, int n) {
+        if (m <= 0 || n <= 0) { return 0; } // out of bounds: 0 paths
+        if (m == 1 && n == 1) { return 1; } // 1x1: 1 path
+
+        return uniquePaths(m-1,n) + uniquePaths(m, n-1);
+    }
+
+    /**
+     * Same as above but uses a cache so we don't have to recalculate duplicate values
+     */
+    public int uniquePathsWithCache(int m, int n) {
+        if (m <= 0 || n <= 0) { return 0; } // out of bounds, 0 paths
+        if (m == 1 && n == 1) { return 1; } // 1x1, 1 path
+        if( cache.get(generateKey(m,n)) != null) {
+            return cache.get(generateKey(m,n));
+        }
+
+        int value = uniquePaths(m-1,n) + uniquePaths(m, n-1);
+        cache.put(generateKey(m,n), value);
+        cache.put(generateKey(n,m), value); // put keys both ways since m x n =  n x m
+        return value;
+    }
+
+    private String generateKey(int m, int n) {
+        return m + "," + n;
+    }
+
+    private static HashMap<String, Integer> cache;
+
+    public P_0062() {
+        cache = new HashMap<>();
+    }
+}
+
+/*
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+
+Example 1:
+
+Input: m = 3, n = 7
+Output: 28
+
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+
+
+Constraints:
+
+    1 <= m, n <= 100
+
+
+*/
---
+++ leet-code/java/tests/P_0062_Tests.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class P_0062_Tests {
+    /**
+     * Parameters for basic tests.
+     */
+    private static Stream<Arguments> basicParameters() {
+        return Stream.of(
+                Arguments.of(3, 7, 28), // (m, n, expected)
+                Arguments.of(3, 2, 3),
+                Arguments.of(1, 1, 1),
+                Arguments.of(0, 1, 0),
+                Arguments.of(0, 0, 0),
+                Arguments.of(1, 0, 0),
+                Arguments.of(2, 2, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicParameters")
+    public void uniquePaths_BasicParameters(int m, int n, int expected) {
+        P_0062 CUT = new P_0062();
+
+        int result  = CUT.uniquePaths(m, n);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicParameters")
+    public void uniquePathsWithCache_BasicParameters(int m, int n, int expected) {
+        P_0062 CUT = new P_0062();
+
+        int result  = CUT.uniquePathsWithCache(m, n);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+}

bcb287f722c66178091db841a44680289923e321
Eisen <eisene@protonmail.com>
2024-02-19 05:19:17 -0800

    Adding test data and a few large tests for the Trie

---
+++ data-structures/java/tests/trees/TrieLargeDictionaryTests.java
@@ -0,0 +1,68 @@
+package trees;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TrieLargeDictionaryTests {
+
+    public static Trie trie;
+
+    @BeforeAll
+    public static void constructTrie() throws IOException {
+        trie = new Trie();
+        String myCurrentDir = System.getProperty("user.dir");
+        Path path = FileSystems.getDefault().getPath(myCurrentDir + "\\tests\\test-data\\words\\english_words.txt");
+        List<String> words = Files.readAllLines(path);
+        for (String word : words) {
+            trie.insert(word);
+        }
+    }
+
+
+    @Test
+    public void wordsWithPrefix_MaxTwoResults_ReturnsTwoResults() {
+        String prefix = "app";
+
+        List<String> result = trie.wordsWithPrefix(prefix, 2);
+
+        assertEquals(2, result.size());
+        for (String word : result) {
+            assertTrue(word.startsWith(prefix));
+        }
+    }
+
+    @Test
+    public void wordsWithPrefix_MaxTenResults_ReturnsTenResults() {
+
+        String prefix = "ba";
+
+        List<String> result = trie.wordsWithPrefix(prefix, 10);
+
+        assertEquals(10, result.size());
+        for (String word : result) {
+            assertTrue(word.startsWith(prefix));
+        }
+    }
+
+    @Test
+    public void wordsWithPrefix_AllWords_ReturnsAllWords() {
+
+        String prefix = "z";
+
+        List<String> result = trie.wordsWithPrefix(prefix);
+
+        assertEquals(101, result.size());
+        for (String word : result) {
+            assertTrue(word.startsWith(prefix));
+        }
+    }
+}

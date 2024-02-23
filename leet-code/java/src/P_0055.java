dd1a42b8fbfa99a1a8c735a45db575db09bdf9e8
Eisen <eisene@protonmail.com>
2024-02-22 16:25:17 -0800

    Add leetcode problem 55: jump game

---
+++ leet-code/java/src/P_0055.java
@@ -0,0 +1,132 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// 55. Jump Game --- https://leetcode.com/problems/jump-game/description
+// See bottom for problem statement
+public class P_0055 {
+
+    /**
+     * Recursively checking all paths.
+     */
+    public boolean canJump(int[] nums) {
+        if (nums[0] >= nums.length-1) { return true; } // we can jump to the end
+        if (nums[0] <= 0) { return false; } // dead end
+
+        for (int i = 0; i < nums[0]; i++) {
+            if (canJump(Arrays.copyOfRange(nums, i+1, nums.length))) { // recursively pass in a smaller subarray
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Same as above but with memoification (i.e. caching already visited indexes). Each time we explore an index fully
+     * we add it to the memo so we don't have to re-explore it.
+     */
+    public boolean canJump2(int[] nums) {
+        return canJumpMemo(nums, 0, new ArrayList<>());
+    }
+
+    private boolean canJumpMemo(int[] nums, int currentIndex, ArrayList<Integer> memo) {
+        if (memo.contains(currentIndex)) { return false; } // already explored this index node
+        if (nums[0] >= nums.length-1) { return true; } // we can jump to the end
+        if (nums[0] <= 0) { return false; } // dead end
+
+        for (int i = 0; i < nums[0]; i++) {
+            if (canJumpMemo(Arrays.copyOfRange(nums, i+1, nums.length), currentIndex+1+i, memo)) { // recursively pass in a smaller subarray
+                return true;
+            }
+        }
+        memo.add(currentIndex);
+        return false;
+    }
+
+    /**
+     * Backtrack method. We start at the end and try to work out way to the beginning to see if anything could jump each step
+     * along the way.
+     */
+    public boolean canJump3(int[] nums) {
+        if (nums.length <= 1) { return true; }
+
+        int backTrackIndex = nums.length-1; // the current index we can jump to the end to (starts from the end and works to the start)
+        for (int i = 1; backTrackIndex-i >= 0; i++) { // we move i back to see if there are any indexes that can jump to the current backTrackIndex
+            int jumpIndex = backTrackIndex-i;
+            if (nums[jumpIndex]+jumpIndex >= backTrackIndex) { // If true we can jump to the backtrack index from the jumpIndex
+                backTrackIndex = jumpIndex; // move the backtrack index forward
+                i = 0; // reset i
+                if (backTrackIndex == 0) {
+                    return true; // we reached beginning starting from the end
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Greedy forward algorithm. We look ahead to the farthest we can jump in TWO jumps and always take that choice.
+     */
+    public boolean canJump4(int[] nums) {
+        if (nums.length <= 1) { return true; }
+
+        int currentIndex = 0;
+        while (true) {
+            int numJumps = nums[currentIndex];
+            int bestJump = 0;
+
+            // Check each possible double jump from the current index and find the best
+            for (int i = currentIndex+numJumps; i > currentIndex; i--) { // check the farthest first jump first
+                if (i >= nums.length-1) {
+                    return true; // if the first jump crosses, we are done
+                }
+
+                if (i + nums[i] > bestJump) {
+                    bestJump = numJumps + nums[i];
+                }
+            }
+
+            currentIndex += bestJump;
+            if (currentIndex >= nums.length-1) {
+                return true; // if the first best jump crosses, we are done
+            }
+
+            if (bestJump <= 0) {
+                return false; // Can no longer jump
+            }
+        }
+    }
+}
+
+/*
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+
+
+Constraints:
+
+    1 <= nums.length <= 104
+    0 <= nums[i] <= 105
+
+
+*/
---
+++ leet-code/java/tests/P_0055_Tests.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class P_0055_Tests {
+    /**
+     * Parameters for basic tests.
+     */
+    private static Stream<Arguments> basicTestParameters() {
+        return Stream.of( // (nums, target, expected)
+                Arguments.of(new int[]{2,3,1,1,4}, true), // e1
+                Arguments.of(new int[]{3,2,1,0,4}, false), // e2
+                Arguments.of(new int[]{1}, true), // 1 element
+                Arguments.of(new int[]{0}, true), // 1 element with a zero
+                Arguments.of(new int[]{1,1,1,1,1,1,1,1}, true), // all 1s
+                Arguments.of(new int[]{1,1,1,1,1,1,1,0}, true), // last 0 doesn't matter
+                Arguments.of(new int[]{1,1,1,1,1,1,0,0}, false), // last 2 are zeroes
+                Arguments.of(new int[]{1,1,1,1,1,2,0,0}, true), // jump the last zero
+                Arguments.of(new int[]{0,1,1,1,1,1,1,1}, false), // start with 0
+                Arguments.of(new int[]{2,0,2,0,3,0,0,1}, true), // barely able to make it
+                Arguments.of(new int[]{100,0,0,0,0,0,0,0}, true), // super jump
+                Arguments.of(new int[]{0,0,0,0,0,0,0,0}, false), // all zeroes
+                Arguments.of(new int[]{5,9,3,2,1,0,2,3,3,1,0,0}, true) // all zeroes
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void canJump_BasicParameters(int[] nums, boolean expected) {
+        P_0055 CUT = new P_0055();
+
+        boolean result  = CUT.canJump(nums);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void canJump2_BasicParameters(int[] nums, boolean expected) {
+        P_0055 CUT = new P_0055();
+
+        boolean result  = CUT.canJump2(nums);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void canJump3_BasicParameters(int[] nums, boolean expected) {
+        P_0055 CUT = new P_0055();
+
+        boolean result  = CUT.canJump3(nums);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void canJump4_BasicParameters(int[] nums, boolean expected) {
+        P_0055 CUT = new P_0055();
+
+        boolean result  = CUT.canJump4(nums);
+
+        Assertions.assertEquals(expected, result);
+    }
+}

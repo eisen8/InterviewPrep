321e89eb2f94600d812cff8565bdf1a5f07218e3
Eisen <eisene@protonmail.com>
2024-02-24 12:14:15 -0800

    Adding leetcode problem 290: Word Pattern

---
+++ leet-code/java/src/P_0290.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+// 290. Word Pattern --- https://leetcode.com/problems/word-pattern/description
+// See bottom for problem statement
+public class P_0290 {
+
+    /**
+     * Split the s String into words. Then keep a running map of pattern characters to words characters. As we go through
+     * the pattern we create the map and verify that the mapping holds true for the entire string.
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = Arrays.stream(s.split(" ")).filter(w -> !w.isEmpty()).toArray(String[]::new);
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        if (pattern.length() <= 1) {
+            return true;
+        }
+
+        // mappings from pattern characters to words  and vice versa
+        Map<Character, String> pMap = new HashMap<>();
+        Map<String, Character> wordMap = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char pChar = pattern.charAt(i);
+            String word = words[i];
+            if (pMap.containsKey(pChar)) {
+                if (!word.equals(pMap.get(pChar))) { // verify mapping holds
+                    return false;
+                }
+            } else if (wordMap.containsKey(word)) {
+                if (pChar != wordMap.get(word)) { // verify mapping holds
+                    return false;
+                }
+            }
+
+            else {
+                pMap.put(pChar, word);
+                wordMap.put(word, pChar);
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+
+
+
+Constraints:
+
+    1 <= pattern.length <= 300
+    pattern contains only lower-case English letters.
+    1 <= s.length <= 3000
+    s contains only lowercase English letters and spaces ' '.
+    s does not contain any leading or trailing spaces.
+    All the words in s are separated by a single space.
+
+
+*/
---
+++ leet-code/java/tests/P_0290_Tests.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class P_0290_Tests {
+    /**
+     * Parameters for basic tests.
+     */
+    private static Stream<Arguments> basicTestParameters() {
+        return Stream.of( // (pattern, s, expected)
+                Arguments.of("abba", "dog cat cat dog", true), // e1
+                Arguments.of("abba", "dog cat cat fish", false), // e2
+                Arguments.of("aaaa", "dog cat cat dog", false), // e3
+                Arguments.of("", "", true), // empty
+                Arguments.of("a", "dog cat ", false), // mixed size
+                Arguments.of("ab", "dog cat", true), // two
+                Arguments.of("abbd", "dog cat cat dog", false), // reused word
+                Arguments.of("abba", "dog dog dog dog", false), // reused word
+                Arguments.of("aaaaaaa", "dog dog dog dog dog dog dog", true),
+                Arguments.of("aaaaaaa", "dog dog dog dog dog dog dog dog", false), // too many dogs
+                Arguments.of("aaaaaaaa", "dog dog dog dog dog dog dog", false) // too many a's
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void wordPattern_BasicParameters(String pattern, String s, boolean expected) {
+        P_0290 CUT = new P_0290();
+
+        boolean result  = CUT.wordPattern(pattern, s);
+
+        Assertions.assertEquals(expected, result);
+    }
+}

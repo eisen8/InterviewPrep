34ba8699f82f077b17c0d0a751ab967475aaa476
Eisen <eisene@protonmail.com>
2024-02-24 10:33:41 -0800

    Adding leetcode problem 205: isomorphic strings

---
+++ leet-code/java/src/P_0205.java
@@ -0,0 +1,84 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+// 205. Isomorphic Strings --- https://leetcode.com/problems/isomorphic-strings/description
+// See bottom for problem statement
+public class P_0205 {
+
+    /**
+     * Keep a running map of s characters to t characters. As we go through the strings we create the map and verify
+     * that the mapping holds true for the entire string.
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     */
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        if (s.length() <= 1) {
+            return true;
+        }
+
+        // mappings from s characters to t characters and vice versa
+        Map<Character, Character> sCharMap = new HashMap<>();
+        Map<Character, Character> tCharMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (sCharMap.containsKey(sChar)) {
+                if (tChar != sCharMap.get(sChar)) { // verify mapping holds
+                    return false;
+                }
+            } else if (tCharMap.containsKey(tChar)) {
+                if (sChar != tCharMap.get(tChar)) { // verify mapping holds
+                    return false;
+                }
+            }
+
+            else {
+                sCharMap.put(sChar, tChar);
+                tCharMap.put(tChar, sChar);
+            }
+        }
+
+        return true;
+    }
+}
+
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+
+
+
+Constraints:
+
+    1 <= s.length <= 5 * 104
+    t.length == s.length
+    s and t consist of any valid ascii character.
+
+
+*/
---
+++ leet-code/java/tests/P_0205_Tests.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class P_0205_Tests {
+    /**
+     * Parameters for basic tests.
+     */
+    private static Stream<Arguments> basicTestParameters() {
+        return Stream.of( // (nums, target, expected)
+                Arguments.of("egg", "add", true), // e1
+                Arguments.of("foo", "bar", false), // e2
+                Arguments.of("paper", "title", true), // e3
+                Arguments.of("", "", true), // empty
+                Arguments.of("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", true), // equivalent
+                Arguments.of("aaa", "abc", false), //
+                Arguments.of("abcdefghijklmnopqrstuvwxyz", "zyxwvutsrqponmlkjihgfedcba", true), // reversed
+                Arguments.of("bbbaaaba", "aaabbbba", false),
+                Arguments.of("badc", "baba", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void isIsomorphic_BasicParameters(String s, String t, boolean expected) {
+        P_0205 CUT = new P_0205();
+
+        boolean result  = CUT.isIsomorphic(s, t);
+
+        Assertions.assertEquals(expected, result);
+    }
+}

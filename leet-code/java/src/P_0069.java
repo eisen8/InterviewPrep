07c9ea90ca9346ff82de07ca664a58749e813aec
Eisen <eisene@protonmail.com>
2024-02-24 19:10:06 -0800

    Adding leetcode problem 69: sqrt(x)

---
+++ leet-code/java/src/P_0069.java
@@ -0,0 +1,78 @@
+// 69. Sqrt(x) --- https://leetcode.com/problems/sqrtx/description
+// See bottom for problem statement
+public class P_0069 {
+
+    /**
+     * Brute force it. Start at 1 and try to find the closest value squared that gives us x.
+     */
+    public int mySqrt(int x) {
+        if (x <= 0) { return 0; }
+
+        int i = 1;
+        while (true) {
+            int squared = i*i;
+
+            if (squared == x) {
+                return i;
+            } else if (squared > x) {
+                return i-1;
+            }
+            i++;
+        }
+    }
+
+    /**
+     * Instead of checking every number in order, do a binary search for the correct number. So we start in the middle and then move the middle
+     * up or down depending on if it is too low or too high.
+     */
+    public int mySqrt2(int x) {
+        if (x <= 0) { return 0; }
+
+        int start = 1;
+        int end = x;
+        while (start <= end) {
+            int mid = start+(end-start)/2;
+            int squared = mid*mid;
+
+            if (squared == x) {
+                return mid;
+            } else if (squared < x) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return end;
+    }
+
+}
+
+/*
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+    For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+
+
+Constraints:
+
+    0 <= x <= 231 - 1
+
+
+*/
---
+++ leet-code/java/tests/P_0069_Tests.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class P_0069_Tests {
+    /**
+     * Parameters for basic tests.
+     */
+    private static Stream<Arguments> basicTestParameters() {
+        return Stream.of( // (x, expected)
+                Arguments.of(4, 2), // e1
+                Arguments.of(8, 2), // e2
+                Arguments.of(0, 0), // zero
+                Arguments.of(1, 1), // one
+                Arguments.of(69, 8),
+                Arguments.of(64, 8),
+                Arguments.of(63, 7),
+                Arguments.of(81, 9)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void mySqrt_BasicParameters(int x, int expected) {
+        P_0069 CUT = new P_0069();
+
+        int result = CUT.mySqrt(x);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("basicTestParameters")
+    public void mySqrt2_BasicParameters(int x, int expected) {
+        P_0069 CUT = new P_0069();
+
+        int result = CUT.mySqrt2(x);
+
+        Assertions.assertEquals(expected, result);
+    }
+}
